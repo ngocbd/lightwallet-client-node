5dbe97e1f129cb9dd028e0dfb0ed60c00d2235ba
NgocBD <buidinhngoc.aiti@gmail.com>
2021-08-26 10:04:07 +0700

    finish core function

---
+++ index.js
@@ -0,0 +1,93 @@
+const axios = require('axios');
+//
+
+class Lightwallet{
+  constructor (key)
+  {
+    this.request = axios.create({
+      baseURL: 'https://lightwallet.appspot.com/',
+      timeout: 10000,
+      headers: {'CB-ACCESS-KEY': key,'Content-Type': 'application/json'}
+    });
+  }
+  
+  version() {
+    return "1.0.0";
+  }
+
+  accounts()
+  {
+    return this.request.get("accounts/?page=1&row=1000");
+  }
+  register(email,password,urlWebhook)
+  {
+    var data = JSON.stringify({
+      "email": email,
+      "password": password,
+      "urlWebhook": urlWebhook,
+      "showPrivateKey": true
+    });
+    return this.request.post("user/register",data);
+  }
+  login(email,password){
+    var data = JSON.stringify({
+      "email": email,
+      "password": password     
+    });
+    return this.request.post("user/login",data);
+  }
+
+  update(email,password,urlWebhook,showPrivateKey)
+  {
+    var data = JSON.stringify({
+      "email": email,
+      "password": password,
+      "urlWebhook": urlWebhook,
+      "showPrivateKey": showPrivateKey
+    });
+    return this.request.post("user/update",data);
+  }
+
+  createAccount(name,currency)
+  {
+    var data = JSON.stringify({
+      "name": name,
+      "primary": true,
+      "currency": currency
+    });
+    return this.request.post("accounts",data);
+  }
+  getAddress(page=1,row=1000)
+  {
+    return this.request.get("accounts/?page="+page+"&row="+row);
+  }
+  getAccount(id)
+  {
+    return this.request.get("accounts/"+id);
+  }
+  send(fromUser,fromAccount,to,amount,currency,data,fee,description)
+  {
+    var data = JSON.stringify({
+      "to": to,
+      "amount": amount,
+      "data": data,
+      "currency": currency,
+      "fee": fee,
+      "description": description
+    });
+
+    return this.request.post("accounts/"+fromUser+"|"+fromAccount+"/transactions",data);
+  }
+  checkHash(hash,currency)
+  {
+    var data = JSON.stringify({
+      "currency": currency,
+      "hash": hash
+    });
+    return this.request.post("hash/check-hash",data);
+
+  }
+}
+
+exports = module.exports = (key) =>{ return new Lightwallet(key)};
+  
---
+++ test/test.js
@@ -0,0 +1,81 @@
+const chai = require('chai')
+const expect = chai.expect
+const lightwallet = require('../')("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJsaWdodHdhbGxldCIsImp0aSI6ImFkbWluLnNvbWVwcm9qZWN0QGdtYWlsLmNvbSJ9.kGVJqoUQzXJSm2FypkSr6on8547bQ68I-Lu4N16ZlKk");
+
+describe("Lightwallet", () => {
+
+	//test a function for a specific case
+	it("should return client version", ()=> {
+		expect(lightwallet.version()).equals("1.0.0");
+	})
+
+
+	it("should return new user or return error Conflict 409", async ()=> {
+		try {
+			let user = await lightwallet.register("admin.someproject@gmail.com","passwordonlyfortest","https://somewhere.com/");
+			
+		} catch (error) {
+			
+			expect(error.response.status).equals(409);
+		}
+
+	})
+
+	it("should return updated user or return error Conflict 409", async ()=> {
+		
+			let result = await lightwallet.update("admin.someproject@gmail.com","passwordonlyfortest","https://somewhere.com/",true);
+			
+			expect(result.data).equal("change notification url success!")
+		
+
+	})
+	it("should return access token", async ()=> {
+		
+			let result = await lightwallet.login("admin.someproject@gmail.com","passwordonlyfortest");
+			expect(result.data).have.property("token")
+		
+
+	})
+	it("should return created account", async ()=> {
+		
+		let result = await lightwallet.createAccount("user"+new Date().getTime(),"TRX");
+		
+		expect(result.data).have.property("id")
+		
+	
+
+	})
+	
+	it("should return account list", async ()=> {
+		let result = await lightwallet.accounts();
+		expect(result.data).to.be.an('array');
+	})
+
+	it("should return address list", async ()=> {
+		let result = await lightwallet.getAddress();
+		expect(result.data).to.be.an('array');
+	})
+
+	it("should return account detail", async ()=> {
+		let result = await lightwallet.getAccount("admin.someproject@gmail.com|user.someproject@gmail.com");
+		expect(result.data).have.property("email")
+	})
+	it("should return transaction detail or 500 error Your balance is not enough to send.", async ()=> {
+		try {
+			
+		let result = await lightwallet.send("admin.someproject@gmail.com","user.someproject@gmail.com","TWCAqe8QtcmaRLvBSfF3YQSFCmq5wusNYU",1,"TRX","{gasValue:3}","0.00063","no description");
+		
+		expect(result.data).have.property("email")	
+		} catch (error) {
+			
+			expect(error.response.data.message).equal("Your balance is not enough to send.");
+			
+		}
+	})
+	
+
+
+
+	
+	
+})
